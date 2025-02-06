28929387e0f35b2a6811664d43de987c6879234b
Fortune <chihurumikechukwu05@gmail.com>
2025-02-06 11:56:53 +0100

    resolve all compile issues

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const Modal = ({ isOpen, closeModal }:{isOpen:boolean,closeModal:()=>void}) => {
   if (!isOpen) return null;
--- src/components/NavigatePeerDetailsPageButton.tsx
+++ src/components/NavigatePeerDetailsPageButton.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const NavigatePeerDetailsPageButton = ({text,details}:{text:string,details:"send" | "receive"}) => {
